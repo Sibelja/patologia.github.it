e4105ec5ce2ec1115de8b368cbb91034da617c4d
Sibelja <109551741+Sibelja@users.noreply.github.com>
2025-11-04 10:09:51 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("omeostatici").onclick = function() {
     document.getElementById("omeostasi").style.background = "yellow";
 };
 
-document.getElementById("sintomoclic").onclick = function() {
+document.getElementById("sintoclic").onclick = function() {
     document.getElementById("sintomo").style.background = "yellow";
 };
 
